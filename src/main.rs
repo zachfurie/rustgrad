2853174289cf5e48afe59303feb34d5e73f87de9
zachfurie <zachfurie0608@gmail.com>
2022-10-20 22:04:43 -0400

    testing speed

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ use std::env;
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
-    match rust_grad_ops::simple() {
-        Ok(x) => {x}
-        Err(e) => println!("ERROR:  {}", e)
-    };
+    rust_grad_ops::get_test();
+    // match rust_grad_ops::simple() {
+    //     Ok(x) => {x}
+    //     Err(e) => println!("ERROR:  {}", e)
+    // };
 }
