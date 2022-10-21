6563f1ad289d8f0bf937836d11a99bbd6589a0e0
zachfurie <zachfurie0608@gmail.com>
2022-10-20 23:41:48 -0400

    10x faster now that I am using arrays instead of vecs to index tensors

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use std::env;
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
-    rust_grad_ops::get_test();
-    // match rust_grad_ops::simple() {
-    //     Ok(x) => {x}
-    //     Err(e) => println!("ERROR:  {}", e)
-    // };
+    // rust_grad_ops::get_test();
+    match rust_grad_ops::simple() {
+        Ok(x) => {x}
+        Err(e) => println!("ERROR:  {}", e)
+    };
 }
