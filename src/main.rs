33282874c4cbbf7aa469bac466113f5897d1ee35
zachfurie <zachfurie0608@gmail.com>
2022-10-21 04:34:57 -0400

    implemented conv2D, maxpooling, relu, dropout. conv2d works correctly for forward and backward in toy test cases, but gets terrible results in training.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::env;
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
-    // rust_grad_ops::get_test();
+    // rust_grad_ops::c2d_test();
     match rust_grad_ops::simple() {
         Ok(x) => {x}
         Err(e) => println!("ERROR:  {}", e)
