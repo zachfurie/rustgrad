ea78c34c087d620d756a8d7a863501db4a5dc339
zachfurie <zachfurie0608@gmail.com>
2022-10-23 19:45:10 -0400

    added multithreading to forward, backward, and to matmul. For some reason matmul is faster with multithreading on forward, but slower on backward

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::env;
 
 
 fn main() {
-    env::set_var("RUST_BACKTRACE", "1");
+    // env::set_var("RUST_BACKTRACE", "1");
     // rust_grad_ops::c2d_test();
     match rust_grad_ops::simple() {
         Ok(x) => {x}
