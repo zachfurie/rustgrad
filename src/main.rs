a7de5f254067100c741c782630be0c2ad5d9ca3b
zachfurie <zachfurie0608@gmail.com>
2022-10-28 10:16:45 -0400

    refactored must stuff to use iterators to be faster and more flexible about shapes. can now use batched inputs with everything

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@
 
 fn main() {
     // env::set_var("RUST_BACKTRACE", "1");
-    // rust_grad_ops::c2d_test();
     match rust_grad_ops::simple() {
         Ok(x) => {x}
         Err(e) => println!("ERROR:  {}", e)
